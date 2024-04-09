b2c82dc918eba788b5a7234ef1aa1396206eafd5
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2024-04-09 19:24:50 +0300

    Update shahm.py

--- shahm.py
+++ shahm.py
@@ -112,5 +112,43 @@ async def OwnerStart(event):
                 chs += 1
                 await event.reply(f"القناة رقم {chs}")
         await abbas.send_message(event.chat_id, "تم الانتهاء من التجميع | SH")
+@abbas.on(events.NewMessage(outgoing=True, pattern='.تجميع دعمكم'))
+async def OwnerStart(event):
+        await event.reply("جاري تجميع النقاط")
+        #await event.reply("جاري تجميع النقاط")
+        #
+        channel_entity = await abbas.get_entity(bot_username)
+        await abbas.send_message(bot_username, '/start')
+        await asyncio.sleep(4)
+        msg0 = await abbas.get_messages(bot_username, limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(4)
+        msg1 = await abbas.get_messages(bot_username, limit=1)
+        await msg1[0].click(0)
+        chs = 1
+        for i in range(100):
+            await asyncio.sleep(4)
+            list = await abbas(GetHistoryRequest(peer=channel_entity, limit=1, offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await shahm1.send_message(event.chat_id, f"تم الانتهاء من التجميع | SH")
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await abbas(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await abbas(ImportChatInviteRequest(bott))
+                msg2 = await abbas.get_messages(bot_username, limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                await event.reply(f"تم الانضمام في {chs} قناة")
+            except:
+                msg2 = await abbas.get_messages(bot_username, limit=1)
+                await msg2[0].click(text='التالي')
+                chs += 1
+                await event.reply(f"القناة رقم {chs}")
+        await abbas.send_message(event.chat_id, f"تم الانتهاء من التجميع | SH\nعدد النقاط المجموعة: {chs}")
 
 abbas.run_until_disconnected()
